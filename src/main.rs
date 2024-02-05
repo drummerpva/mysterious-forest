5a47e34c0de0cd09d50a7a270a37ce9c75e48b4f
Douglas Poma <douglaspoma@yahoo.com>
2024-02-05 10:33:20 -0300

    feat: first verifications

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    let mut pontuacao: i32 = 0;
+    let escolha: i32 = 1;
+
+    println!("Bem-vindo à floresta misteriosa!");
+    println!("Escolha sua próxima ação");
+    println!("1 - Entrar na caverna escura");
+    println!("2 - Seguir pelo caminho iluminado");
+    println!("3 - Cruzar a ponte frágil");
+    println!("4 - Descansar na beira do riacho");
+
+    if escolha == 1 {
+        pontuacao += 50;
+    }
+    if escolha == 2 {
+        pontuacao -= 20;
+    }
+    if escolha == 3 {
+        pontuacao -= 20;
+    }
+    if escolha == 4 {
+        pontuacao += 10;
+    }
+
+    println!("Sua escolha foi {}", escolha);
+    println!("Sua pontuação foi {}", pontuacao);
 }
